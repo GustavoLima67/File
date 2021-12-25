5c63720ffe763d158900b654a500b2e70f0d5086
Gustavo <lgusta333@gmail.com>
2021-12-25 20:11:38 -0300

    Commit finalized

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,10 @@ package application;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -15,41 +15,54 @@ import java.util.Scanner;
 import entities.Product;
 
 public class Program {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws ParseException {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
 		List<Product> list = new ArrayList<>();
+
+		System.out.println("Enter file path: ");
+		String sourceFileStr = sc.nextLine();
+
+		File sourceFile = new File(sourceFileStr);
+		String sourceFolderStr = sourceFile.getParent();
 		
-		System.out.print("Enter the path: ");
-		String sourceFile = sc.nextLine();
+		boolean success = new File(sourceFolderStr + "\\out").mkdir();
 		
-		File file = new File(sourceFile);
-		String sourceFolder = file.getParent();
-		
-		boolean success = new File(sourceFolder+"\\out").mkdir();
-		String targetFile = sourceFolder + "\\out\\summary.csv";
-		
-		try(BufferedReader br = new BufferedReader(new FileReader(sourceFolder))){
-			
+		String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+
 			String itemCsv = br.readLine();
-			while(itemCsv != null) {
-				String fields[] = itemCsv.split(", ");
+			while (itemCsv != null) {
+
+				String[] fields = itemCsv.split(",");
 				String name = fields[0];
 				double price = Double.parseDouble(fields[1]);
 				int quantity = Integer.parseInt(fields[2]);
-				
+
 				list.add(new Product(name, price, quantity));
-				
+
 				itemCsv = br.readLine();
 			}
+
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+
+				for (Product item : list) {
+					bw.write(item.getName() + "," + String.format("%.2f", item.total()));
+					bw.newLine();
+				}
+
+				System.out.println(targetFileStr + " CREATED!");
+				
+			} catch (IOException e) {
+				System.out.println("Error writing file: " + e.getMessage());
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
 		}
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter(targetFile))) {
-			
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-		}
+
 		sc.close();
 	}
-
 }
